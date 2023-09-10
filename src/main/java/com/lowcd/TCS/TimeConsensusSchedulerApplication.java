db7430adbabaaba6718d6e3176674f2d518787d9
srivatsav06 <srivatsav184536@gmail.com>
2023-09-11 03:36:13 +0530

    created boiler plate code for springboot project

---
+++ src/main/java/com/lowcd/TCS/TimeConsensusSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.lowcd.TCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeConsensusSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeConsensusSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lowcd/TCS/TimeConsensusSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowcd.TCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeConsensusSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
