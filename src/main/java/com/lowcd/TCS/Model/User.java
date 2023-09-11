560bbaa4fd8c8a4a0cede5f5435c3d4d97cdece0
Harry <nath23927@gmail.com>
2023-09-11 19:23:22 +0530

    create User and Role Models

---
+++ src/main/java/com/lowcd/TCS/Model/User.java
@@ -0,0 +1,27 @@
+package com.lowcd.TCS.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer userid;
+
+    @Column(name="name",nullable = false)
+    private  String name;
+
+    @Column(name="password",nullable = false)
+    private String password;
+
+    @Column(name="email",nullable = false)
+    private String email;
+}
