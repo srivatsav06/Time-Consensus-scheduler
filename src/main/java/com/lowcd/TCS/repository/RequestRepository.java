0e55a35c2833f0129af43ad7fdb8535b1a6e7447
rahul <rahulreddysama.05@gmail.com>
2023-09-16 11:45:07 +0530

    Add Create and Get APIs in request controller, Add utility class, Add code review script

--- src/main/java/com/lowcd/TCS/repository/RequestRepository.java
+++ src/main/java/com/lowcd/TCS/repository/RequestRepository.java
@@ -1,4 +1,10 @@
 package com.lowcd.TCS.repository;
+import com.lowcd.TCS.entity.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public class RequestRepository {
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+    List<Request> findRequestsByParticipantsUserid(Long userId);
 }
