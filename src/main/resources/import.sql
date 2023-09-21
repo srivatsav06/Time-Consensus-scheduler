3d6dc2dfe9706989f46251e11e014d372f5720b8
rahul <rahulreddysama.05@gmail.com>
2023-09-21 16:12:42 +0530

    Made changes to import.sql to use seq.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,17 +8,17 @@ INSERT INTO roles (roleId, roleName) VALUES (1, 'student');
 INSERT INTO roles (roleId, roleName) VALUES (2, 'teacher');
 
 -- Insert the first user
-INSERT INTO users (userid,name, password, email, role_fk) VALUES (1,'User1 Name', 'User1Password', 'user1@example.com', 2);
+INSERT INTO users (userid,name, password, email, role_fk) VALUES (nextval('users_seq') ,'User1 Name', 'User1Password', 'user1@example.com', 2);
 
 -- Insert the second user
-INSERT INTO users (userid,name, password, email, role_fk) VALUES (2,'User2 Name', 'User2Password', 'user2@example.com', 1);
+INSERT INTO users (userid,name, password, email, role_fk) VALUES (nextval('users_seq') ,'User2 Name', 'User2Password', 'user2@example.com', 1);
 
 -- Insert a new request
-INSERT INTO requests (reqid, dateandtime, status, title, description) VALUES (1, '2023-09-17 10:00:00', 'PENDING', 'Title 1', 'Description 1');
+INSERT INTO requests (reqid, dateandtime, status, title, description) VALUES (nextval('requests_seq'), '2023-09-17 10:00:00', 'PENDING', 'Title 1', 'Description 1');
 
-INSERT INTO participantrequests (reqid, userid) VALUES (1, 1),(1, 2);
+INSERT INTO participantrequests (reqid, userid) VALUES (1, 1),(1, 51);
 
 --Insert a new event
-INSERT INTO events (eventid, title, description, dateandtime) VALUES (1, 'Sample Event', 'This is a sample event description', '2023-09-16 15:30:00');
+INSERT INTO events (eventid, title, description, dateandtime) VALUES (nextval('events_seq'), 'Sample Event', 'This is a sample event description', '2023-09-16 15:30:00');
 
-INSERT INTO eventparticipants (eventid, userid) VALUES (1, 1), (1, 2);
+INSERT INTO eventparticipants (eventid, userid) VALUES (1, 1), (1, 51);
